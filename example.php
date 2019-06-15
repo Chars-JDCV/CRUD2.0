c34e1cc1ebd43791d6fa612887910164dd86f803
Ch4rs <campeador.david@gmail.com>
2019-06-15 08:12:06 -0700

    new

---
+++ example.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+</head>
+<body>
+    <input id="u"   type="button" value="Arriba">
+    <input id="d"   type="button" value="abajo">
+    <input id="l"   type="button" value="izquierda">
+    <input id="r"   type="button" value="derecha">
+
+</body>
+<script src="assets/js/jquery-3.1.1.min.js"></script>
+<script src="main.js"></script>
+</html>
---
+++ main.js
@@ -0,0 +1,14 @@
+$("#u").click(adelante());
+$("#d").click(function() {   
+    console.log("d")
+});
+$("#l").click(function() {
+   console.log("l")
+});
+$("#r").click(function() {
+   console.log("r")
+});
+
+function adelante() {
+    console.log('u')
+}
